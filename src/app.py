430e5041acd5cb71cc4462172ae0219dc2bb4d8c
AldoGomezz <55323506+AldoGomezz@users.noreply.github.com>
2022-08-27 08:54:32 -0500

    Login y SignUp con roles
    Ahora se crean tres tipos diferentes de usuarios, al momento del login aparece una pantalla de acuerdo al tipo de usuario

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ app.secret_key='qwerty'
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='dbtp2'
+app.config['MYSQL_DB']='antiphish_db'
 
 mysql = MySQL(app)
 
@@ -18,19 +18,21 @@ mysql = MySQL(app)
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     mesage=''
-    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form and 'rol' in request.form:
         email = request.form['email']
         password = request.form['password']
+        rol= request.form['rol']
         cursor=mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM administrador_global WHERE correoAG = % s AND passwordAG = % s', (email, password, ))
+        cursor.execute('SELECT * FROM administrador_global WHERE correoAG = % s AND passwordAG = % s AND rol = % s',(email, password, rol, ))
         administrador_global = cursor.fetchone()
         if administrador_global: 
             session['loggedin']=True
             session['name']=administrador_global['idAG']
             session['username']=administrador_global['nombreAG']
             session['email']=administrador_global['correoAG']
+            session['rol']=administrador_global['rol']
             mesage = 'Bienvenido'
-            return render_template('user.html', mesage=mesage)
+            return redirect(url_for('user'))
         else:
             mesage='Por favor ingrese un email o contraseña correcta'
     return render_template('login.html', mesage = mesage)
@@ -42,13 +44,24 @@ def logout():
     session.pop('email', None)
     return redirect(url_for('login'))
 
+@app.route('/user')
+def user():
+    if 'email' in session and session['rol'] == "Administrador Global":
+        return render_template('user.html')
+    elif 'email' in session and session['rol']== "Administrador de Red":
+        return "Hola, esta es la pantalla para el admin de red"
+    else:
+        return "Hola, esta es la pantalla para el usuario"
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     mesage=''
-    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form:
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form and 'rol' in request.form:
         userName=request.form['name']
         password=request.form['password']
         email=request.form['email']
+        rol=request.form['rol']
         cursor=mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM administrador_global WHERE correoAG = % s', (email, ))
         account=cursor.fetchone()
@@ -56,17 +69,19 @@ def register():
             mesage='Cuenta ya Existe'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             mesage='Email invalido'
-        elif not userName or not password or not email:
+        elif not userName or not password or not email or not rol:
             mesage='Por favor inserte el dato que falta'
         else:
-            cursor.execute('INSERT INTO administrador_global VALUES (NULL,% s, % s, % s)', (userName, email, password, ))
+            cursor.execute('INSERT INTO administrador_global VALUES (NULL,% s, % s, % s, % s)', (userName, email, password, rol, ))
             mysql.connection.commit()
             mesage='Cuenta creada satisfactoriamente'
-        return render_template('user.html', mesage=mesage)
+        return render_template('login.html', mesage=mesage)
 
     elif request.method == 'POST':
         mesage='Por favor complete el formulario'
     return render_template('register.html', mesage=mesage)
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
