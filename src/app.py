37e62e6db030710d3dd98ff827086ccced3e75bd
pry20220144tp2 <pry20220144tp2@gmail.com>
2022-09-12 21:54:03 -0500

    j

--- src/app.py
+++ src/app.py
@@ -108,8 +108,10 @@ def register():
         account = cursor.fetchone()
         if account:
             mesage = 'Cuenta ya Existe'
+        elif not re.match(r'[a-zA-ZÀ-ÿ\\u00f1\\u00d1]+(\\s[a-zA-ZÀ-ÿ\\u00f1\\u00d1])*[a-zA-ZÀ-ÿ\\u00f1\\u00d1]+$', userName):
+            mesage = 'El nombre solo puede tener letras y máximo 1 palabra'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            mesage = 'Correo electrónico inválido'
+            mesage = 'Ingrese sus datos / datos inválidos'
         elif not userName or not password or not email or not rol:
             mesage = 'Por favor inserte el dato que falta'
         else:
