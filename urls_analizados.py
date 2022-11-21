3c88d40acf3bd731a6b1951728542c7284c0c8e4
pry20220144tp2 <pry20220144tp2@gmail.com>
2022-11-21 18:14:18 -0500

    Revert "Update urls_analizados.py"
    This reverts commit 20c2563121e111778dcd45af192bc9bdd1cc9f26.

--- urls_analizados.py
+++ urls_analizados.py
@@ -13,7 +13,7 @@ def url_search(url):
         file_path = 'URLs Analizados.csv'
         if os.path.getsize(file_path) == 0:
 
-            header = ['URL', 'ESTADO']
+            header = ['URL', 'ESTADO','CARACTERISTICAS']
             writeCSV = csv.writer(f,delimiter=',')
             writeCSV.writerow(header)
             flag='False'
@@ -41,11 +41,12 @@ def url_search(url):
     else:   
         return 'NOT FOUND'
 
-def url_update(url,result):
+def url_update(url,result,list):
        
     data = []
     data.append(url)
     data.append(result)
+    data.append(list)
    
     with open('URLs Analizados.csv','a',newline='') as csvfile:
                 
